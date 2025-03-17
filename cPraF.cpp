fd774f4b389e367480471fe00e102a9ad7cd59f9
Rafael Yohanan Farina <ra2457013>
2025-03-17 20:17:40 -0300

    boa

---
+++ cPraF.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    float c;
+    printf("Digite a temperatura em Celsius: ");
+    scanf("%f", &c);
+    float cPraf = c*1,8+32;
+    printf("Digite a temperatura em %.2f: ",cPraf);
+	getchar();    
+    float cPraK = c+273.15;
+    printf("Digite a temperatura em %.2f: ",cPraK);
+	getchar();
+}
